9f68898d078303150b5f8d25a22cac633bcd94d0
shastkivRuslan <shastkiv.ruslan.dev@gmail.com>
2025-01-22 19:08:26 +0200

    minor changes

--- src/main/java/com/project/opportunities/controller/admin/AdminTelegramController.java
+++ src/main/java/com/project/opportunities/controller/admin/AdminTelegramController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/telegram")
 @RequiredArgsConstructor
-public class TelegramController {
+public class AdminTelegramController {
     private final TelegramService telegramService;
 
     @Operation(
